300eae93322d7b6ce252246dcaf9631ccf3894d1
taikoiv <alex.grieco4@hotmail.com>
2017-03-21 09:31:21 +0100

    defined library functions

--- heap-library/mHeap.h
+++ heap-library/mHeap.h
@@ -7,6 +7,13 @@ typedef struct{
 	int *data;	
 } heap;
 
-
+int isEmpty(heap h);
+int size(heap h);
+int min(heap h);
+void insert(heap *h,int k);
+void delete(heap *h,int k);
+heap* buildHeap(int* data,int size);
+void heapSort(heap *h);
+void heapify(heap *h,int i);
 
 #endif
