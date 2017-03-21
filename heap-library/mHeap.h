5d176cf2cdbd86ed125013fed52b95d86ffe1c7e
taikoiv <alex.grieco4@hotmail.com>
2017-03-21 09:35:39 +0100

    style fix

--- heap-library/mHeap.h
+++ heap-library/mHeap.h
@@ -7,9 +7,9 @@ typedef struct{
 	int *data;	
 } heap;
 
-int isEmpty(heap h);
-int size(heap h);
-int min(heap h);
+int isEmpty(heap *h);
+int size(heap *h);
+int min(heap *h);
 void insert(heap *h,int k);
 void delete(heap *h,int k);
 heap* buildHeap(int* data,int size);
