db7d61742ff8930c9ceb7266d1b2556640542add
taikoiv <alex.grieco4@hotmail.com>
2017-03-29 13:23:41 +0200

    pointer method and position function
    position method creates coordinates ---- we can use it to build argument
    for left, right and parent methods

--- tableau-library/tableau.c
+++ tableau-library/tableau.c
@@ -6,20 +6,33 @@ typedef struct{
 	int x,y;
 } coordinates;
 
-coordinates left(coordinates i){
-	coordinates cs;
-	cs.x=i.x+1;
-	cs.y=i.y;
+coordinates* position(int i,int j);
+coordinates* parent(tableau *t,coordinates* i);
+coordinates* left(coordinates *i);
+coordinates* right(coordinates *i);
+
+coordinates* position(int i,int j){
+	coordinates *cs=(coordinates*) malloc(sizeof(coordinates));
+	cs->x=i;
+	cs->y=j;
 	
 	return cs;
 }
 
-coordinates right(coordinates i){
-	coordinates cs;
-	cs.x=i.x;
-	cs.y=i.y+1;
+coordinates* left(coordinates *i){
+	coordinates* cs=(coordinates*) malloc(sizeof(coordinates));
+	cs->x=(i->x)+1;
+	cs.y=i->y;
 	
 	return cs;
 }
 
-coordinates parent(tableau *t,coordinates i);
+coordinates* right(coordinates *i){
+	coordinates* cs=(coordinates*) malloc(sizeof(coordinates));
+	cs->x=i->x;
+	cs->y=(i->y)+1;
+	
+	return cs;
+}
+
+
