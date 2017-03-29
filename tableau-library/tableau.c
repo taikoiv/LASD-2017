ea2d41c4d8ca3e13b9c1291fa7d846fcc4770020
taikoiv <alex.grieco4@hotmail.com>
2017-03-29 14:50:50 +0200

    coordinates operations fixes

--- tableau-library/tableau.c
+++ tableau-library/tableau.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "tableau.h"
 
-typedef struct{
+typedef struct{ // X => RIGA | Y=>COLONNA
 	int x,y;
 } coordinates;
 
-coordinates* position(int i,int j);
-coordinates* parent(tableau *t,coordinates* i);
-coordinates* left(coordinates *i);
-coordinates* right(coordinates *i);
+coordinates* position(int i,int j); //COSTRUISCE E RESTITUISCE COORDINATES
+coordinates* parent(tableau *t,coordinates* i); //RESTITUISCE NULL SE NON ESISTONO, ARRAY di 1 elemento se ce ne è 1, ARRAY di 2 se è figlio di puttana
+coordinates* left(tableau *t,coordinates *i);//RESTITUISCE COORDINATE DEL FIGLIO SINISTRO O NULL SE NON ESISTE
+coordinates* right(tableau *t,coordinates *i);//RESTITUISCE COORDINATE DEL FIGLIO DESTRO O NULL SE NON ESISTE
 
 coordinates* position(int i,int j){
 	coordinates *cs=(coordinates*) malloc(sizeof(coordinates));
@@ -19,20 +20,43 @@ coordinates* position(int i,int j){
 	return cs;
 }
 
-coordinates* left(coordinates *i){
-	coordinates* cs=(coordinates*) malloc(sizeof(coordinates));
-	cs->x=(i->x)+1;
-	cs.y=i->y;
-	
+coordinates* left(tableau *t,coordinates *i){
+	int dim=sqrt(t->size);
+	coordinates* cs=NULL;
+	if((i->x)+1<=dim){
+		cs=(coordinates*) malloc(sizeof(coordinates));
+		cs->x=(i->x)+1;
+		cs->y=i->y;
+	}
 	return cs;
 }
 
-coordinates* right(coordinates *i){
-	coordinates* cs=(coordinates*) malloc(sizeof(coordinates));
-	cs->x=i->x;
-	cs->y=(i->y)+1;
-	
+coordinates* right(tableau *t,coordinates *i){
+	int dim=sqrt(t->size);
+	coordinates* cs=NULL;
+	if((i->y)+1<=dim){
+		cs=(coordinates*) malloc(sizeof(coordinates));
+		cs->x=i->x;
+		cs->y=(i->y)+1;
+	}
 	return cs;
 }
 
+coordinates* parents(tableau *t,coordinates *i){
+	coordinates *cs=NULL;
+	int dim=sqrt(t->size);
+	if(i->x>1 && i->y>1){
+		cs=(coordinates*) malloc(2*sizeof(coordinates));
+		cs[0]=position((i->x)-1,i->y);
+		cs[1]=position(i->x,(i->y)-1);
+	}else{
+		cs=(coordinates*) malloc(sizeof(coordinates))
+		if(i->x>1)
+			cs=position((i->x)-1,i->y);
+		if(i->y>1)
+			cs=position(i->x,(i->y)-1);
+	}
+	
+	return cs;
+}
 
