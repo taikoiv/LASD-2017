8c7e1c6e8a29a40bd8ac5798e79fbe3f5db28328
camillazampella <cam.zampella@studenti.unina.it>
2017-04-11 13:51:43 +0200

    beta stack main

--- stack-library/main.c
+++ stack-library/main.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
 #include "stack.h"
 
@@ -18,7 +15,6 @@
 #endif
 
 /* THIS FILE IS JUST AN EXAMPLE THAT EXPLAIN HOW TO USE mHeap LIBRAY */
-int ERROR;
 void printMenu(); //PRINT PRIMARY MENU
 void printOpMenu(); //PRINT SECONDARY MENU
 
@@ -27,17 +23,29 @@ void clearBuffer(){ //clear the stdin when user write an invalid input data
 	while ((c = getchar()) != '\n' && c != EOF) { };
 }
 
+queue *createFromInput(int n){
+	int i = 0, val = 0;
+	stack *s = NULL;
+	s = newStack();
+	for(; i<n; i++){
+		scanf("%d", &val);
+	    push(s, val);
+	}
+	return q;
+}
+		
+
 int main(int argc, char *argv[]) {
-	int choice=-1,value;
-	int min;
+	int choice=-1, value;
+	int element, n;
 	srand(time(NULL));
 	stack *s = NULL;
 
-	printf("Welcome to min Tableau library example \n\n");
-	while(choice<0 || choice>4){
+	printf("Welcome to stack library example \n\n");
+	while(choice<0 || choice>3){
 		printMenu();
 		scanf("%d",&choice);
-		if(choice<0 || choice>4){
+		if(choice<0 || choice>3){
 			clearBuffer();
 			printf("NOT A VALID VALUE \n");
 			system(PAUSE);
@@ -48,21 +56,18 @@ int main(int argc, char *argv[]) {
 	switch(choice){
 		case 0 : printf("Goodbye\n");
 			return 0;
-		case 1 : h=getTabFromInput(1);
+		case 1 : q = newStack();
 			break;
-		case 2 : h=getTabFromInput(0);
+		case 2 : 
+				 printf("How many elements do you wish to insert?\n");
+				 scanf("%d", &n);
+				 s = createFromInput(n);
 			break;
-		case 3 : h=getTabFromRandomVal();
-			break;
-        case 4 :sortArray();
-            system(PAUSE);
-            return 0;
-			break;
-		default:
+		case 3 : s = randomStack();
 			break;
 	}
 	choice=-1;
-	printf("\n\nTableau is created, now let's do operations on it\n");
+	printf("\n\nYour stack is created, now let's do operations on it\n");
 	system(PAUSE);
 	system(CLS);
 	while(choice!=0){
@@ -73,64 +78,62 @@ int main(int argc, char *argv[]) {
 			case 1 : printf("Enter the value to insert : ");
 				scanf("%d",&value);
                 clearBuffer();
-				insert(h,value);
+				push(s,value);
 				break;
-			case 2 : value=-1;
+			case 2 : element = pull(s);
+					 system(CLS);
+					 printf("%d\n", element);
+					 break;
+			case 3: value=-1;
 				do{
 					printf("Press 1 to continue or 0 to cancel\n");
 					scanf("%d",&value);
 					clearBuffer();
 				}while(value!=0 && value!=1);
 				if(value==1){
-					freetableau(h);
-					printf("Tableau successfully deleted\n");
+					free(s);
+					printf("Stack successfully deleted\n");
 					printf("Goodbye\n");
 					return 0;
 				} else choice=-1;
 				break;
-			case 3 : printf("Tableau has %d elements \n",size(h));
-				break;
-			case 4 : if(isEmpty(h))
-					printf("Tableau is empty\n");
+			case 4 : if(isEmpty(s))
+					printf("Stack is empty\n");
 				else
-					printf("Tableau is not empty\n");
+					printf("Stack is not empty\n");
 				break;
-			case 5 : 
-					min = extractMin(h);
-					if(TABLERROR==-2){
-					 	printf("Error: Tableau is empty, you can't extact any more elements\n");
-					 	TABLERROR=0;
-					}else{
-						printf("Min value in the tableau : %d\n",min);
-					}			
-				break;
-			case 6 : printTableau(h);
+			case 5 : printQueue(s);
 				break;
 			default : printf("NOT A VALID VALUE \n");
 				clearBuffer();
 		}
-        if(TABLERROR==-1){
-            printf("Error: Tableau is full, you can't add any more elements\n");
-            TABLERROR=0;
+        if(S_ERROR==-2){
+            printf("Error: Queue is full, you can't add any more elements\n");
+            Q_ERROR=0;
             system(PAUSE);
             system(CLS);
-        }else {
+        }else if (S_ERROR==-1){
+        	printf("Error: Stack is empty, you can't extract any more elements\n");
+            S_ERROR=0;
+            system(PAUSE);
+            system(CLS);
+		}else {
             if (choice != 0) system(PAUSE);
             system(CLS);
         }
 	}
 	printf("Goodbye\n");
-	if(h) freetableau(h);
+	if(s)
+		free(s);
 	return 0;
 }
 
 void printMenu(){ //PRINT PRIMARY MENU
 	printf("\t\t\t\tENTER A CHOICE\n");
 	printf("*====================================================================================*\n");
-	printf("1) Build a tableau with input values\n");
-    printf("2) Build an empty tableau\n");
-    printf("3) Build a tableau with random values\n");
-    printf("4) YoungSort!\n");
+	printf("1) Build an empty stack\n");
+    printf("2) Build a stack with elements given in input\n");
+    printf("3) Build a stack with random elements\n");
     printf("--------------------------------------------------------------------------------------\n");
 	printf("0) Exit\n");
 	printf("*====================================================================================*\n");
@@ -140,105 +143,12 @@ void printOpMenu(){ //PRINT SECONDARY MENU
 	printf("\t\t\t\tENTER A CHOICE\n");
 	printf("*====================================================================================*\n");
 	printf("1) Insert an element\n");
-	printf("2) Delete tableau\n");
-	printf("3) Get tableau size\n");
-	printf("4) Check if tableau is empty\n");
-	printf("5) Get min value\n");
-	printf("6) Print tableau\n");
+	printf("2) Extract an element\n");
+	printf("3) Delete stack\n");
+	printf("4) Check if stack is empty\n");
+	printf("5) Print stack\n");
 	printf("--------------------------------------------------------------------------------------\n");
 	printf("0) Exit\n");
 	printf("*====================================================================================*\n");
 }
 
-void sortArray(){
-    int *array=NULL;
-    int i,dim=0;
-    do{
-        printf("What's the sequence size?\n");
-        scanf("%d",&dim);
-        clearBuffer();
-    }while(dim<0);
-
-    array=malloc(dim*sizeof(int));
-    for(i=0;i<dim;i++){
-        printf("Element %d : ",i+1);
-        scanf("%d",&array[i]);
-        clearBuffer();
-    }
-    youngSort(array,dim);
-    printf("\n| ");
-    for(i=0;i<dim;i++)
-        printf("%d | ",array[i]);
-    printf("\n");
-}
-
-
-tableau* getTabFromRandomVal(){ //BUILD A RANDOM HEAP WITH RANDOM DIMENSION AND VALUES
-    int r,c,i,tot,*array;
-    tableau* h=NULL;
-    r=rand();
-    c=rand();
-    tot = rand()%(r*c);
-    array = (int *) malloc((c * r) * sizeof(int));
-    for(i=0;i<(c*r);i++)
-        array[i]=rand();
-        
-    h = createTableau(array,r,c,tot);
-    free(array);
-    return h;
-}
-
-
-tableau* getTabFromInput(int mode){ // GET HEAP VALUES AND DIMENSION FROM STDIN
-	int r,c,i,tot,*array,tmp;
-	tableau *h=NULL;
-	r=-1;
-	c =-1;
-	tot = -1;
-	while(r<0){
-		printf("How many rows should the tableau have?\n");
-		scanf("%d",&r);
-		clearBuffer();
-	}
-	while(c<0){
-		printf("How many columns should the tableau have?\n");
-		scanf("%d",&c);
-		clearBuffer();
-	}
-	array = (int *) malloc((c * r) * sizeof(int));
-	if(mode==AUTOINS) {
-		while (tot > c * r || tot < 0) {
-			printf("How many elements to insert?\n");
-			scanf("%d", &tot);
-			clearBuffer();
-
-		}
-		for (i = 0; i < tot; i++) {
-			printf("Element %d ", i + 1);
-			scanf("%d", &array[i]);
-			clearBuffer();
-		}
-	}else{
-		tot = 0;
-	}
-	h=createTableau(array,r,c,tot);
-	free(array);
-	return h;
-}
-
-
-
-int main(int argc, const char * argv[]) {
-    int err = 0;
-    Stack Sp = initStack();
-    Stack Sd = initStack();
-    riempiStack(Sp);
-    riempiStack(Sd);
-    //randomStack(Sp, 20, &err);
-    printStack(Sp, &err);
-    // insert code here...
-    printf("\n Hello, World!\n");
-    return 0;
-}
-
-}
