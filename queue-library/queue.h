4e80685c6d427ebb13a5c0289d7aad32153fc183
taikoiv <alex.grieco4@hotmail.com>
2017-04-11 10:26:05 +0200

    queue comments in header file

--- queue-library/queue.h
+++ queue-library/queue.h
@@ -5,16 +5,22 @@
 
 typedef struct {
   int head,tail;
-  int *data;
+  int data[MAX];
+  //int *data for dynamic method
 } queue;
 
-extern int Q_ERROR
+extern int Q_ERROR //CONTAINS AN ERROR CODE
+/*
+  ERROR CODES:
+    -1 : EMPTY QUEUE (IT IS NOT POSSIBLE TO DEQUEUE)
+    -2 : FULL QUEUE (IT IS NOT POSSIBLE TO ENQUEUE)
+*/
 
-queue* newQueue();
-int isEmpty(queue* q);
-void enqueue(queue* q,int k);
-int dequeue(queue* q);
-void printQueue(queue* q);
-queue* randomQueue();
+queue* newQueue(); //INITIALIZE A QUEUE
+int isEmpty(queue* q); //CHECK IF QUEUE IS EMPTY (RETURN 0 IF NOT, ELSE 1)
+void enqueue(queue* q,int k); //ADD AN ELEMENT IN THE QUEUE
+int dequeue(queue* q); //EXTRACT AN ELEMENT FROM THE QUEUE
+void printQueue(queue* q); //PRINT THE QUEUE
+queue* randomQueue(); //CREATE A QUEUE WITH RANDOM VALUES
 
 #endif
