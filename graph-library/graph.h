86bbd0205da331bfa40c86b4bbe0c218be259493
taikoiv <alex.grieco4@hotmail.com>
2017-05-23 09:29:44 +0200

    start graph library
    yeeee

---
+++ graph-library/graph.h
@@ -0,0 +1,31 @@
+#ifndef GRAPH_H
+#define GRAPH_H
+
+struct edge{
+	int weight;
+	int node;
+	struct edge* next;
+};
+
+typedef struct edge edge;
+
+struct graph {
+	edge** adj;
+	int n; 
+};
+
+typedef struct graph graph;
+
+graph* createGraph();
+graph* createRandomGraph();
+void insertNode(graph* g);
+void deleteNode(graph* g,int node);
+void addEdge(graph* g,int s, int d);     //NOT ORIENTED MODE
+void deleteEdge(graph* g,int s, int d);  //NOT ORIENTED MODE
+void addOEdge(graph* g,int s, int d);    //ORIENTED MODE
+void deleteOEdge(graph* g,int s, int d); //ORIENTED MODE
+void BFS(graph* g,int* s);
+void DFS(graph* g);
+graph* transpose(graph* g);
+
+#endif
