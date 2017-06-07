7870423c222ec1ec1890fcec2bf1a8430e1d4872
camillazampella <cam.zampella@studenti.unina.it>
2017-06-07 15:24:46 +0200

    priority queues

--- matteo-project/queue.c
+++ matteo-project/queue.c
@@ -18,12 +18,23 @@ int parent(int i){
 }
 
 int isEmpty(heap *h){
-	return (h->data[0]==1);
+	return (h->size==0);
+}
+
+void swap(heap *h, int s, int t){
+	float temp;
+	int sw = 0;
+	temp = h->data[s].priority;
+	h->data[s].priority = h->data[t].priority;
+	h->data[t].priority = temp;
+	sw = h->pos[h->data[s].id];
+	h->pos[h->data[s].id] = h->pos[h->data[t].id];
+	h->pos[h->data[t].id] = sw;
 }
 
 void heapify(heap *h, int i){ //restore heap properties
 	if(!isEmpty(h)){
-		int min = i, scambio =0, scambiopos = 0;
+		int min = i;
 		int l = left(i);
 		int r = right(i);
 		if(l <= size(h) && h->data[l]<h->data[i])
@@ -31,12 +42,7 @@ void heapify(heap *h, int i){ //restore heap properties
 		if(r <= size(h) && h->data[r]<h->data[min])
 			min = r;
 		if(min!=i){
-			scambio = h->data[i];
-			h->data[i] = h->data[min];
-			h->data[min] = scambio;
-			scambiopos = h->pos[i];
-			h->pos[i] = h->pos[min];
-			h->pos[min] = scambiopos;
+			swap(h, i, min);
 			heapify(h, min);
 		}
 	}
@@ -46,9 +52,11 @@ heap *createHeap(int n){
 	heap *h = NULL;
 	h = (heap *)malloc(sizeof(heap));
 	if(h!=NULL){
-		h->size = n;
-		h->data = (int *)malloc((h->size+1)*sizeof(int));
-		h->pos = (int *)malloc((h->size+1)*sizeof(int));
+		h->size = 0;
+		h->allocated = n;
+		h->data = (node *)malloc((n)*sizeof(node));
+		h->pos = (int *)malloc((n)*sizeof(int));
+		
 	}
 	else{
 		HEAP_ERROR = -1;
@@ -56,12 +64,56 @@ heap *createHeap(int n){
 	return h;
 }
 
+int climbheap(heap *h, int i) { //swap element with his parent until is possible
+	if (i!=0 && h->data[i] < h->data[parent(i)]) {
+		swap(h, i, parent(i));
+		return climbheap(h, parent(i));
+	}else{
+		return i;
+	}
+}
+
 void insert(heap *h, int k, float f){
 	if(h!=NULL){
-		h->data[0]++;
-		h->data[h->data[0]] = f;
-		heapify(h, climbheap(h, size(h)));
+		h->size++;
+		h->data[h->size].priority = f;
+		h->data[h->size].id = k;
+		h->pos[k] = h->data[h->size];
+		climbheap(h, h->size));
 	}
+}
+
+
+
+int extractMin(heap *h){
+	int ret = -1;
+	if(!isEmpty(h)){
+		ret = h->data[0].id;
+		swap(h, 0, h->size);
+		h->pos[h->data[h->size].id] = -1;
+		h->size--;
+		heapify(h, 0);
+	}
+	return ret;
+}
+
+void update(heap *h, int k, float p){
+	temp = h->data[h->pos[k]].priority;
+	h->data[h->pos[k]].priority = p;
+	if(h!=0){
+		if(temp > p)
+			climbheap(h, h->pos[k]);
+		else
+			heapify(h, h->pos[k]);
+	}else{
+		HEAP_ERROR = -2;
+	}
+}
+
+void freeheap(heap *h){
+	free(heap->pos);
+	free(h->data);
+	free(h);
 }
 
 
--- matteo-project/queue.h
+++ matteo-project/queue.h
@@ -2,9 +2,15 @@
 #define QUEUE_H_INCLUDED
 
 
+typedef struct node{
+	float priority;
+	int id;
+};
+
 typedef struct{
 	int size; //the real size of the array used to represent the heap
-	float *data;
+	int allocated;
+	node *data;
 	int *pos;
 }heap;
 
@@ -13,7 +19,6 @@ extern int HEAP_ERROR;
 int isEmpty(heap *h);					// IF HEAP CONTAINS 0 ELEMENTS RETURN 0 ELSE RETURN 1
 int extractMin(heap *h);						// RETURN THE MINIMUM VALUE IN THE HEAP OR 0 IF HEAP IS EMPTY
 void insert(heap *h,int k,float p);				// ADD k TO THE HEAP
-void delete(heap *h,int k);
 heap *createHeap(int n);
 void update(heap *h, int k, float p);
 void freeheap(heap *h);
